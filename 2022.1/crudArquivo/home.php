e82835a003cb7c90d084d94e344c7fe3e4c39a57
IgorQuintarelli <58370687+IgorQuintarelli@users.noreply.github.com>
2022-05-13 17:08:02 -0300

    Add files via upload

---
+++ 2022.1/crudArquivo/home.php
@@ -0,0 +1,16 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+    <h1>Home</h1>
+    <br>
+    <a href="incluirProduto.php">Incluir Produto</a><br>
+    <a href="alterarProduto.php">Alterar Produto</a><br>
+    <a href="listarTodosProdutos.php">Listar Todos os Produto</a><br>
+    <a href="exibirUmProduto.php">Exibir Um Produto</a><br>
+    <a href="excluirProduto.php">Excluir Produto</a><br>
+</body>
+</html>
